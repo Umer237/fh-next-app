a18df2b30afb4eb0524c3426cdfce34f5b1885cd
Umer237 <umerafzal981@gmail.com>
2023-06-02 17:04:58 +0500

    umer

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,40 @@
-
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import '../styles/Home.module.css'
+import "../styles/Home.module.css";
 import Home from "./pages/Home";
-import About from './pages/About'
+import About from "./pages/About";
 import Footer from "./pages/Footer";
 import Services from "./pages/Services";
-
-function index () {
-
-  
-const [page2, SetPage2] = useState(<Home/>)
-const [page1, SetPage] = useState('')
-
-
-const handleOnClick = () => { 
-  SetPage((<Services/>))
-  SetPage2((<About/>))
-  // SetPage2((<Services/>))
-}
-
+function index() {
+  const [page2, SetPage2] = useState(<Home />);
+  const [page1, SetPage] = useState("");
+  const handleOnClick = () => {
+    SetPage2(<About />);
+  };
+  const handleupClick = () => {
+    SetPage(<Services />);
+  };
   return (
-  <>
-     <div className="NavBar">
+    <>
+      <div className="NavBar">
         <nav>
-          <ul>      
-<Image src='/img/FH.png' width='45' height='60' alt="logo"/>
-<button >HOME</button>
-<button onClick={handleOnClick}>ABOUT</button>
-<button>SERVICES</button>
-<button>CONTACT</button>
-<button>TEAM</button>
-<button>PORTFOLIOS</button>
+          <ul>
+            <Image src="/img/FH.png" width="45" height="60" alt="logo" />
+            <button>HOME</button>
+            <button onClick={handleOnClick}>ABOUT</button>
+            <button onClick={handleupClick}>SERVICES</button>
+            <button>CONTACT</button>
+            <button>TEAM</button>
+            <button>PORTFOLIOS</button>
           </ul>
         </nav>
       </div>
       {page2}
-   {/* <Home value={page1} /> */}
-  {/* <Footer/> */}
-</>
-
-  )
+      {/* <Home value={page1} /> */}
+      {/* <Footer/> */}
+    </>
+  );
 }
 
-export default index
+export default index;
